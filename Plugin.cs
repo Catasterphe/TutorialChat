728d0f55efd2529437d17b4f0407313d1a7e3a6f
Catasterphe <26910433+Catasterphe@users.noreply.github.com>
2023-11-14 16:00:36 -0500

    Update version (to match game version) + reference files for update v13.3.1

--- Plugin.cs
+++ Plugin.cs
@@ -7,12 +7,12 @@ namespace TutorialChat
     public class Plugin
     {
         public static Plugin Singleton { get; private set; }
-        public const string Version = "1.0.1";
+        public const string Version = "13.3.1";
         [PluginConfig] public Config Config;
 
         [PluginEntryPoint("Tutorial Chat", Version, "A recreation of the swiftkraft command that lets tutorials/admins chat with .chat", "Aster")]
         public void LoadPlugin()
-        {
+        { 
             Singleton = this;
             EventManager.RegisterEvents<EventHandler>(this);
             var handler = PluginHandler.Get(this);
